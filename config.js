3cc31bf69ed48ba5d10b00a8f1bff8283e0c039e
davidz91800 <davidz91800@gmail.com>
2026-03-12 21:51:12 +0100

    Set real Drive config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.AAR_READER_CONFIG = {
   autoSyncOnStartup: true,
   googleDrive: {
     apiKey: "AIzaSyA0rrL__Ah7rfAXFcHhZzp1JY-EsuzeSz4",
-    folderId: "1uMqVNCwc82BFdw2Wq2wFw3-zhUkpMI0a?usp=sharing",
+    folderId: "1uMqVNCwc82BFdw2Wq2wFw3-zhUkpMI0a",
     indexFileId: ""
   }
 };
