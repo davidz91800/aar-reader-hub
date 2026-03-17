2fcd37d251e494a1383ed60e922dbd7e8d464bf3
davidz91800 <davidz91800@gmail.com>
2026-03-17 09:09:43 +0100

    Switch hubs to Drive-only with persistent local cache

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 window.AAR_READER_CONFIG = {
   autoSyncOnStartup: true,
   googleDrive: {
-    oauthClientId: "",
-    apiKey: "",
-    folderId: "",
+    oauthClientId: "100011978859-sc6aj28as11aqeqmrubrb4rccocvqe9r.apps.googleusercontent.com",
+    apiKey: "AIzaSyAIOITquStWBYg6eLA0hPR7etSct16u2ts",
+    folderId: "18RTzOZzYWEIFWS5NXyYA_Ts3Xyf2X5kX",
     indexFileId: ""
   },
   staticRepo: {
-    enabled: true,
+    enabled: false,
     indexUrl: "./AAR Reader Data/index.json"
   }
 };
